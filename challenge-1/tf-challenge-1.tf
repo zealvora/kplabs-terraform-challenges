ac3d0022d6c838efd26d09d3bbde26a5f5704d39
Zeal Vora <instructors@kplabs.in>
2023-09-30 09:30:13 +0530

    Update tf-challenge-1.tf
    Fixed Challenge 1 Issue.

--- challenge-1/tf-challenge-1.tf
+++ challenge-1/tf-challenge-1.tf
@@ -1,21 +1,17 @@
 provider "aws" {
-  region  = "us-east-1"
+  version = "~> 2.54"
+  region  = "us-east-1"
+  access_key = "AKIAIOSFODNN7EXAMPLE"
+  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 }
+
+provider "digitalocean" {}
 
 terraform {
-  required_providers {
-    digitalocean = {
-      source = "digitalocean/digitalocean"
-    }
-  }
+    required_version = "0.12.31"
 }
-
 
 
 resource "aws_eip" "kplabs_app_ip" {
-  domain   = "vpc"
+  vpc      = true
 }
-
-
-
-
